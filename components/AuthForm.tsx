dc1880c14322719673812c1c8641094f60881d46
Nazakat Umrani - NixOS Linux <unazakat70@gmail.com>
2024-09-20 20:40:40 +0500

    Implement PlaidLink and Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -24,6 +24,7 @@ import CustomInput from './CustomInput';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { signUp, signIn } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
 
 const AuthForm = ({ type }: { type: string }) => {
         const router = useRouter();
@@ -47,7 +48,19 @@ const AuthForm = ({ type }: { type: string }) => {
                 try {
                         // Signup with appwrite and create plaid token
                         if(type==='sign-up'){
-                                const newUser = await signUp(data);
+                                const userData = {
+                                        firstName: data.firstName!,
+                                        lastName: data.lastName!,
+                                        address1: data.address1!,
+                                        city: data.city!,
+                                        state: data.state!,
+                                        postalCode: data.postalCode!,
+                                        dateOfBirth: data.dateOfBirth!,
+                                        ssn: data.ssn!,
+                                        email: data.email,
+                                        password: data.password
+                                }
+                                const newUser = await signUp(userData);
                                 setUser(newUser);
                         }
                         if(type==='sign-in'){
@@ -88,7 +101,7 @@ const AuthForm = ({ type }: { type: string }) => {
                         </header>
                         {user ? (
                                 <div className='flex flex-col gap-4'>
-                                        {/* PlaidLink */}
+                                        <PlaidLink user={user} variant="primary"/>
                                 </div>
                         ) : (
                                 <>
---
+++ components/PlaidLink.tsx
@@ -0,0 +1,52 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { Button } from './ui/button'
+import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from 'react-plaid-link'
+import { useRouter } from 'next/navigation'
+import { createLinkToken, exchangePublicToken } from '@/lib/actions/user.actions'
+
+const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+        const router = useRouter();
+        const [token, setToken] = useState('');
+        useEffect(() => {
+                const getLinkToken = async () => {
+                        const data = await createLinkToken(user);
+
+                        setToken(data?.link_token);
+                }
+
+                getLinkToken();
+        }, [user]);
+        const onSuccess = useCallback<PlaidLinkOnSuccess>(async (public_token: string) => {
+                await exchangePublicToken({
+                        publicToken: public_token,
+                        user,
+                })
+                router.push('/');
+        }, [user])
+        const config: PlaidLinkOptions = {
+                token,
+                onSuccess
+        }
+
+        const { open, ready } = usePlaidLink(config);
+
+        return (
+                <>
+                        {variant === "primary" ? (
+                                <Button className='plaidlink-primary' onClick={() => open()} disabled={!ready}>
+                                        Connect bank
+                                </Button>
+                        ) : variant === "ghost" ? (
+                                <Button>
+                                        Connect a bank account
+                                </Button>
+                        ) : (
+                                <Button>
+                                        Connect a bank account
+                                </Button>
+                        )}
+                </>
+        )
+}
+
+export default PlaidLink
